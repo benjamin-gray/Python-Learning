2601b3f3757184b0cb5f8f178c8a74433890d6e5
Benjamin Gray <benjamin.gray3@yahoo.com>
2025-04-02 23:42:44 -0400

    day_six + reboorg

---
+++ day_6/day_six_functions.py
@@ -0,0 +1,14 @@
+#Functions 
+# Have already been using several functions already, such as print(), input(), and len().
+#Functions bundle instructions together to avoid repetition and make code more readable.
+
+def my_function(): #(def)ining the function
+    print("Hello & Goodbye!") # this is the code that will run when the function is called, this code must be indented. 
+
+my_function() # calling the function, which will run the code in the function
+
+#test your code as you go, and do not wait till the end 
+
+#indentation is highly important to know which block the code belongs to
+
+#spaces vs tabs... python says spaces over tabs
---
+++ day_6/day_six_whileLoop.py
@@ -0,0 +1,21 @@
+#While Loops
+
+# For loops are used to iterate over a sequence (like a list, tuple, or string) or other iterable objects.
+
+# for item in list_of_items:
+#    Do something to each item
+
+#for number in range(a,b):
+#    print(number)
+
+# While loops are used to execute a block of code repeatedly as long as a condition is true.
+
+# while something_is_true:
+#    Do something repeatedly
+
+# while not at_place():
+#    Do something repeatedly
+
+#When to use a while vs for loop. 
+# For loops are great iterating over something, and doing something to eat item. Or for using a range of numbers.
+# While loops are great for doing something until a condition is met. 
